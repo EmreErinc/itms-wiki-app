a7be3b06788e2619a13e8c29ecd1e6bc865a5314
emre <emre_erinc@hotmail.com>
2020-05-09 19:04:53 +0300

    jwt test

--- src/main/java/com/itms/wikiapp/common/security/JwtTokenUtil.java
+++ src/main/java/com/itms/wikiapp/common/security/JwtTokenUtil.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 @Component
 public class JwtTokenUtil implements Serializable {
 
-  public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+  public static final long JWT_TOKEN_VALIDITY = 60;
 
   private String secret = "itmsProjectJwtSecret";
 
